2727db8ae54534714b8617a052ecec7832ff8ed3
Devanshu7828 <devanshurajak23@gmail.com>
2021-05-06 19:46:51 +0530

    server update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require("path");
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const morgan = require("morgan");
 const AppError = require("./utils/appError");
 const errorHandler = require("./controller/errorController");
